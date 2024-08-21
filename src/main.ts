b7e229284edcb16e57889b68b4682d7daa819129
Asplla <wangxu_cn@icloud.com>
2024-08-22 00:48:50 +0800

    IPTV v1.0

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
-import './assets/main.css'
+import './assets/global.css'
 
 import { createApp } from 'vue'
+import TDesign from 'tdesign-vue-next'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+// 引入组件库的少量全局样式变量
+import 'tdesign-vue-next/es/style/index.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(TDesign)
 app.use(router)
 
 app.mount('#app')
