117c331399dff741313ee71029daf2b6bdb7ed07
Asplla <wangxu_cn@icloud.com>
2024-08-22 14:14:13 +0800

    IPTV v1.0

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
 import './assets/global.css'
 
 import { createApp } from 'vue'
-import TDesign from 'tdesign-vue-next'
+import TDesign from 'tdesign-vue-next';
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
 
 // 引入组件库的少量全局样式变量
-import 'tdesign-vue-next/es/style/index.css'
+import 'tdesign-vue-next/es/style/index.css';
+
+import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(TDesign)
+app.use(TDesign);
 app.use(router)
 
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+    meta: {
+      title: "wxhub iptv",
+    },
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue'),
+    meta: {
+      title: "Xu's System Status",
+    },
+  }
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes
+})
+
+router.beforeEach((to,from,next)=>{
+  //beforeEach是router的钩子函数，在进入路由前执行
+  if(to.meta.title){
+    //判断是否有标题
+    document.title = String(to.meta.title)
+  }
+  //执行进入路由，如果不写就不会进入目标页
+  next()
 })
 
 export default router
